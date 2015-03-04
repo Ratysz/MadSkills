3a0968b3523b3ea5c92c9a5e2e9bb8dc64beb46c
Ratys <ratysz@gmail.com>
2015-03-04 12:22:26 +0300

    Proof-of-concept MapComponent injector

---
+++ CompRTMapComponentAdder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace RTMadSkills
+{
+    class CompRTMapComponentAdder : ThingComp
+    {
+        public override void PostSpawnSetup()
+        {
+            if (Find.Map.GetComponent<MapComponent_RTSkillIncreaser>() != null)
+            {
+                return;
+            }
+            else
+            {
+                Find.Map.components.Add(new MapComponent_RTSkillIncreaser());
+                parent.Destroy(DestroyMode.Kill);
+            }
+        }
+    }
+}
