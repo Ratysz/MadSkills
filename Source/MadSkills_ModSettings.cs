2339b77f0f7583625325dcae6bf6cbb51c87854d
Ratys <alexander.sepity@gmail.com>
2017-05-27 13:43:22 +0300

    Redefined multiplier in options as percentage.

--- Source/MadSkills_ModSettings.cs
+++ Source/MadSkills_ModSettings.cs
@@ -6,16 +6,18 @@ namespace RTMadSkills
 	public class MadSkills_ModSettings : ModSettings
 	{
 		public bool tiered = false;
-		public float multiplier = 0.0f;
+		public int multiplierPercentage = 0;
 
 		public override void ExposeData()
 		{
+			float multiplier = multiplierPercentage / 100.0f;
 			base.ExposeData();
 			Scribe_Values.Look(ref tiered, "tiered");
 			Scribe_Values.Look(ref multiplier, "multiplier");
 			MadSkills_Patch.ApplySettings(tiered, multiplier);
 			Log.Message("[MadSkills]: settings initialized, multiplier is " + multiplier
 				+ ", " + (tiered ? "tiered" : "not tiered") + ".");
+			multiplierPercentage = Mathf.RoundToInt(multiplier * 100);
 		}
 
 		public string SettingsCategory()
@@ -25,15 +27,26 @@ namespace RTMadSkills
 
 		public void DoSettingsWindowContents(Rect rect)
 		{
-			string buffer = multiplier.ToString();
+			string buffer = multiplierPercentage.ToString();
 			Listing_Standard list = new Listing_Standard(GameFont.Small);
 			list.ColumnWidth = rect.width / 3;
 			list.Begin(rect);
 			list.Gap();
-			list.Label("MadSkills_MultiplierLabel".Translate());
-			list.TextFieldNumeric(ref multiplier, ref buffer, 0.0f, 100.0f);
-			list.Label("MadSkills_MultiplierTip".Translate());
-			list.GapLine();
+			{
+				Rect rectLine = list.GetRect(Text.LineHeight);
+				Rect rectLeft = rectLine.LeftHalf().Rounded();
+				Rect rectRight = rectLine.RightHalf().Rounded();
+				Rect rectPercent = rectRight.RightPartPixels(Text.LineHeight);
+				rectRight = rectRight.LeftPartPixels(rectRight.width - Text.LineHeight);
+				Widgets.DrawHighlightIfMouseover(rectLine);
+				TooltipHandler.TipRegion(rectLine, "MadSkills_MultiplierTip".Translate());
+				TextAnchor anchorBuffer = Text.Anchor;
+				Text.Anchor = TextAnchor.MiddleLeft;
+				Widgets.Label(rectLeft, "MadSkills_MultiplierLabel".Translate());
+				Text.Anchor = anchorBuffer;
+				Widgets.TextFieldNumeric(rectRight, ref multiplierPercentage, ref buffer, 0, 10000);
+				Widgets.Label(rectPercent, "%");
+			}
 			list.CheckboxLabeled(
 				"MadSkills_TieredLabel".Translate(),
 				ref tiered,
