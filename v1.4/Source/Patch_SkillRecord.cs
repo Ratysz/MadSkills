bea63a41d446ef3bb43ba86225baca8bb5a1122a
Ratys <alexander.sepity@gmail.com>
2022-10-21 18:24:38 -0500

    Update to 1.4; fix up Mod Manager manifest.

---
+++ v1.4/Source/Patch_SkillRecord.cs
@@ -0,0 +1,91 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using Verse;
+
+namespace RTMadSkills
+{
+	[HarmonyPriority(Priority.HigherThanNormal)]
+	[HarmonyPatch(typeof(SkillRecord))]
+	[HarmonyPatch("Interval")]
+	internal static class Patch_SkillRecordInterval
+	{
+		private static FieldInfo pawnField = AccessTools.Field(typeof(SkillRecord), "pawn");
+
+		private static bool Prefix(SkillRecord __instance)
+		{
+			if (!ModSettings.tiered || __instance.XpProgressPercent > 0.1f)
+			{
+				float greatMemMultiplier = (ModSettings.greatMemoryAltered || !(pawnField.GetValue(__instance) as Pawn).story.traits.HasTrait(TraitDefOf.GreatMemory)) ? 1f : 0.5f;
+				float xpToLearn = greatMemMultiplier * VanillaMultiplier(__instance.levelInt) * ModSettings.multiplier;
+				if (xpToLearn != 0.0f)
+				{
+					__instance.Learn(xpToLearn, false);
+				}
+			}
+			return false;
+		}
+
+		private static float VanillaMultiplier(int level)
+		{
+			switch (level)
+			{
+				case 10: return -0.1f;
+				case 11: return -0.2f;
+				case 12: return -0.4f;
+				case 13: return -0.6f;
+				case 14: return -1.0f;
+				case 15: return -1.8f;
+				case 16: return -2.8f;
+				case 17: return -4.0f;
+				case 18: return -6.0f;
+				case 19: return -8.0f;
+				case 20: return -12.0f;
+				default: return 0.0f;
+			}
+		}
+	}
+
+	[HarmonyPatch(typeof(SkillRecord))]
+	[HarmonyPatch("LearnRateFactor")]
+	internal static class Patch_SkillRecordLearnRateFactor
+	{
+		private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			var _instructions = instructions.MethodReplacer(
+				AccessTools.Property(typeof(SkillRecord), nameof(SkillRecord.LearningSaturatedToday)).GetGetMethod(),
+				AccessTools.Method(typeof(Patch_SkillRecordLearnRateFactor), nameof(Patch_SkillRecordLearnRateFactor.LearningSaturatedToday))
+			);
+			var patched = false;
+			foreach (var instruction in _instructions)
+			{
+				if (!patched && instruction.opcode == OpCodes.Ldc_R4 && System.Convert.ToSingle(instruction.operand) == 0.2f)
+				{
+					patched = true;
+					yield return new CodeInstruction(OpCodes.Call, AccessTools.PropertyGetter(typeof(ModSettings), nameof(ModSettings.saturatedXPMultiplier)));
+					continue;
+				}
+				yield return instruction;
+			}
+		}
+
+		private static bool LearningSaturatedToday(SkillRecord instance)
+		{
+			return instance.xpSinceMidnight > ModSettings.dailyXPSaturationThreshold;
+		}
+	}
+
+	[HarmonyPatch(typeof(SkillRecord))]
+	[HarmonyPatch("LearningSaturatedToday", MethodType.Getter)]
+	internal static class Patch_SkillRecordLearningSaturatedToday
+	{
+		private static bool Prefix(SkillRecord __instance, ref bool __result)
+		{
+			__result = __instance.xpSinceMidnight > ModSettings.dailyXPSaturationThreshold;
+			return false;
+		}
+	}
+}
---
+++ v1.4/Source/Patch_SkillUi.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Text;
+using Verse;
+
+namespace RTMadSkills
+{
+	[HarmonyPatch(typeof(SkillUI))]
+	[HarmonyPatch("GetSkillDescription")]
+	internal static class Patch_LearningSaturationUI
+	{
+		private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			int patchState = 0;
+			foreach (var instruction in instructions)
+			{
+				if (patchState == 0 && instruction.opcode == OpCodes.Ldc_I4 && System.Convert.ToInt32(instruction.operand) == 4000)
+				{
+					patchState++;
+					yield return new CodeInstruction(OpCodes.Ldsfld, AccessTools.Field(typeof(ModSettings), nameof(ModSettings.dailyXPSaturationThreshold)));
+					yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(System.Convert), nameof(System.Convert.ToInt32), new System.Type[] { typeof(float) }));
+					continue;
+				}
+				else if (patchState == 1 && instruction.opcode == OpCodes.Ldc_R4 && System.Convert.ToSingle(instruction.operand) == 0.2f)
+				{
+					patchState++;
+					yield return new CodeInstruction(OpCodes.Call, AccessTools.PropertyGetter(typeof(ModSettings), nameof(ModSettings.saturatedXPMultiplier)));
+					continue;
+				}
+				yield return instruction;
+			}
+		}
+	}
+}
